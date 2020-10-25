c42a9db3bcc8b4c9890a7d2592aa4de01a3c9b12
profdetech <emmanuel.mouronval@gmail.com>
2020-10-25 09:25:41 +0100

    Mise à jour pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,48 @@
+//% weight=1001 color=#00aced icon="\uf128"
+namespace LED_RACE_description{
+/**
+	 * P0 = haut parleur --
+	 * P14 = non utilise --
+	 * P1 = bouton joueur 1 --
+	 * P15 = non utilise --
+	 * P2 = bouton joueur 2 --
+	 * P16 = non utilise --
+	 * P12 = Ruban LED --
+	 * P13 = non utilise --
+	 * P8 = non utilise --
+	 */
+    //% blockId=descriptif
+    //% block="Descriptif branchement"
+	//% weight=99 blockGap=24
+    export function descriptif(): void {
+    }
+}
+
+//% weight=1000 color=#00aced icon="\uf128"
+namespace LED_RACE_communiquer{
+
+    /**
+	 *initialisation du ruban led sur P12
+	 */
+    //% blockId=led_init
+    //% block="led initialisation"
+	//% weight=99 blockGap=24
+    export function led_init(): void {
+        let strip = neopixel.create(DigitalPin.P12, 30, NeoPixelMode.RGB)        
+    }
+
+  /**
+	 *initialisation du ruban led sur P12
+	 */
+    //% blockId=led_joueur1
+    //% block="afficher LED joueur 1"
+	//% weight=99 blockGap=24
+    export function led_joueur1(): void {
+        strip.setPixelColor(0, 0)        
+    }
+
+
+
+
+}
 
